bec67d7324e2e4608d89c63d41f91ec97b4989da
nicole elezra <im.nicole.m@gmail.com>
2022-12-28 11:16:03 +0200

    ads.servcie: added scrape and parse functions

--- server/ads.controller.js
+++ server/ads.controller.js
@@ -3,12 +3,12 @@ const adsService = require('./ads.service');
 module.exports = { getAdsData };
 
 async function getAdsData(req, res) {
-  const domain = req.query;
-
-  if (!domain) return res.json({ error, message: 'No domain was provided' });
+  const domain = req.query.data;
+  console.log('domain in controller', domain);
 
   try {
-    const adsData = await adsService.getData(domain);
+    const adsTxt = await adsService.scrapeAdsTxt(domain);
+    const adsData = await adsService.parseAdsTxt(adsTxt);
     res.send(adsData);
   } catch (err) {
     res.status(500).send({ err: 'Failed getting ads data' });
--- server/ads.service.js
+++ server/ads.service.js
@@ -1,7 +1,34 @@
+const puppeteer = require('puppeteer');
+
 module.exports = {
-  getData,
+  scrapeAdsTxt,
+  parseAdsTxt,
 };
 
-async function getData(domain) {
+async function scrapeAdsTxt(domain) {
   console.log('domain', domain);
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://' + domain + '/ads.txt');
+    const txt = await page.evaluate(() => document.querySelector('pre').innerText);
+    await browser.close();
+    return txt;
+  } catch (err) {
+    console.log('err');
+  }
+}
+
+async function parseAdsTxt(txt) {
+  const adsTxt = {};
+
+  const txtLines = txt.split('\n');
+  txtLines.forEach((line) => {
+    const lineDetails = line.split(',');
+    const advertiserDomain = lineDetails[0].toLowerCase();
+    if (lineDetails.length > 1)
+      adsTxt[advertiserDomain] = adsTxt[advertiserDomain] ? adsTxt[advertiserDomain] + 1 : 1;
+  });
+
+  return Object.entries(adsTxt);
 }
