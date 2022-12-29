c2af98363cebb78cd8410132930c4d0d04426bf6
nicole elezra <im.nicole.m@gmail.com>
2022-12-29 21:32:48 +0200

    deleted comments

--- server.js
+++ server.js
@@ -8,25 +8,20 @@ const http = require('http').createServer(app);
 app.use(express.json());
 app.use(express.static('public'));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.resolve(__dirname, 'public')));
-// } else {
-//   const corsOptions = {
-//     origin: [
-//       'http://127.0.0.1:8080',
-//       'http://localhost:8080',
-//       'http://127.0.0.1:3000',
-//       'http://localhost:3000',
-//     ],
-//     credentials: true,
-//   };
-//   app.use(cors(corsOptions));
-// }
-app.use(
-  cors({
-    origin: 'http://localhost:3000' || 'https://ads-parser.onrender.com/',
-  })
-);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'public')));
+} else {
+  const corsOptions = {
+    origin: [
+      'http://127.0.0.1:8080',
+      'http://localhost:8080',
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+    ],
+    credentials: true,
+  };
+  app.use(cors(corsOptions));
+}
 
 const adsRoutes = require('./server/ads.routes');
 
