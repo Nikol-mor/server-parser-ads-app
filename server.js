68020eaf96b241e6ba3c46c9908fa6be9ba4cc4b
nicole elezra <im.nicole.m@gmail.com>
2022-12-29 17:25:57 +0200

    added cors option

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ if (process.env.NODE_ENV === 'production') {
       'http://localhost:8080',
       'http://127.0.0.1:3000',
       'http://localhost:3000',
+      'https://ads-parser.onrender.com/',
     ],
     credentials: true,
   };
